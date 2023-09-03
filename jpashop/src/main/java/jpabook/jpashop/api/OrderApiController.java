2d3e5cb5fe844fd5b62ab23b444c7b12a167542e
nohyunha95 <nohyunha95@gmail.com>
2023-09-03 21:22:58 +0900

    API 최적화 권장순서

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -22,6 +22,19 @@ import java.util.List;
 
 import static java.util.stream.Collectors.*;
 
+/**
+ * 권장 순서
+ * 1. 엔티티 조회 방식으로 우선 접근
+ *      1. 페치조인으로 쿼리 수를 최적화
+ *      2. 컬렉션 최적화
+ *          1. 페이징 필요 hibernate.default_batch_fetch_size, @BatchSize로 최적화
+ *          2. 페이징 필요X -> 페치 조인 사용
+ * 2. 엔티티 조회 방식으로 해결이 안되면 DTO 조회 방식 사용
+ * 3. DTO 조회 방식으로 해결이 안되면 NativeSQL or 스프링 JdbcTemplate
+ *
+ * 캐싱할 시점이 오면 엔티티를 직접 캐싱하지 말고 DTO로 변환해서 캐싱한다. (캐쉬에 올라가면 엔티티가 삭제되지 않는 현상 발생)
+ *
+ */
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
